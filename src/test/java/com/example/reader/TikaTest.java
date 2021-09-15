3b10988058f9fafb1fbe20cc218cc98afd54352e
likai <327381633@qq.com>
2021-09-15 10:12:32 +0800

    新增测试类，以及Junit依赖

---
+++ src/test/java/com/example/reader/TikaTest.java
@@ -0,0 +1,27 @@
+package com.example.reader;
+
+
+import com.example.reader.tikademo.TikaTool;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Map;
+
+public class TikaTest {
+
+    @Test
+    public void getObject() throws FileNotFoundException {
+        FileInputStream inputStream = new FileInputStream(new File("D:\\test\\Data Provider Questionnaire_Chinese.xlsx"));
+        Map<String, Object> map = TikaTool.parseFile(inputStream);
+        System.out.println(map.get("content"));
+    }
+
+    @Test
+    public void getString() throws FileNotFoundException {
+        FileInputStream inputStream = new FileInputStream(new File("D:\\test\\Data Provider Questionnaire_Chinese.xlsx"));
+        String content = TikaTool.parseFileContent(inputStream);
+        System.out.println(content);
+    }
+}
